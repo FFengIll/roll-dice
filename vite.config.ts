caf672f056eda75c4ba21abac4c38a1ac39e380a
FFengIll <fengyouzheng@gmail.com>
2024-11-14 22:36:46 +0800

    build: set to use relative-path to build and ref static.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: './', // 设置为相对路径
 })
