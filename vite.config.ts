7eed211bace1553b72ad9ab2d3fda6fcf2068133
FFengIll <fengyouzheng@gmail.com>
2025-04-18 17:39:30 +0800

    config: open dist path as vite default.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     base: './', // 设置为相对路径
+    server: {
+        host: 'localhost',
+        port: 3000,
+        open: '/roll-dice/dist', // 默认打开的页面
+    }
 })
